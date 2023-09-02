355fe3974e215991080d58043693c6ad88349f98
orbashan94 <orbashan94@gmail.com>
2023-09-02 12:07:56 +0300

    Dashboard Home Page

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -3,9 +3,56 @@ import React from 'react'
 function Home() {
   return (
     <div>
-      Home
+      <div className="p-3 d-flex justify-content-around mt-3">
+        <div className="px-3 pt-2 pb-3 border shadow-sm w-25">
+          <div className="text-center pb-1">
+            <h4>Admin</h4>
+          </div>
+          <hr />
+          <div className="">
+            <h5>Total: {}</h5>
+          </div>
+        </div>
+        <div className="px-3 pt-2 pb-3 border shadow-sm w-25">
+          <div className="text-center pb-1">
+            <h4>Employee</h4>
+          </div>
+          <hr />
+          <div className="">
+            <h5>Total: {}</h5>
+          </div>
+        </div>
+        <div className="px-3 pt-2 pb-3 border shadow-sm w-25">
+          <div className="text-center pb-1">
+            <h4>Salary</h4>
+          </div>
+          <hr />
+          <div className="">
+            <h5>Total: {}</h5>
+          </div>
+        </div>
+      </div>
+
+      {/* list of admins */}
+      <div className="mt-4 px-5 pt-3">
+        <h3>List of Admins</h3>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Email</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Admin</td>
+              <td>Admin</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home
